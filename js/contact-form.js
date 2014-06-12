627cfbcc119b29d711b89a3ede6cb3aeda77c556
zane-insight <zstatz@insightcreative.com>
2014-06-12 09:48:53 -0500

    form on contact page

---
+++ js/contact-form.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $("#feedbackSubmit").click(function() {
+    var $btn = $(this);
+    $btn.button('loading');
+    //clear any errors
+    contactForm.clearErrors();
+
+    //do a little client-side validation -- check that each field has a value and e-mail field is in proper format
+    var hasErrors = false;
+    $('#feedbackForm input,textarea').not('.optional').each(function() {
+      if (!$(this).val()) {
+        hasErrors = true;
+        contactForm.addError($(this));
+      }
+    });
+    var $email = $('#email');
+    if (!contactForm.isValidEmail($email.val())) {
+      hasErrors = true;
+      contactForm.addError($email);
+    }
+
+    var $phone = $('#phone');
+    if (!contactForm.isValidPhone($phone.val())) {
+      hasErrors = true;
+      contactForm.addError($phone);
+    }
+
+    //if there are any errors return without sending e-mail
+    if (hasErrors) {
+      $btn.button('reset');
+      return false;
+    }
+
+    //send the feedback e-mail
+    $.ajax({
+      type: "POST",
+      url: "../library/sendmail.php",
+      data: $("#feedbackForm").serialize(),
+      success: function(data) {
+        contactForm.addAjaxMessage(data.message, false);
+        contactForm.clearForm();
+        //get new Captcha on success
+        $('#captcha').attr('src', '../library/vender/securimage/securimage_show.php?' + Math.random());
+      },
+      error: function(response) {
+        contactForm.addAjaxMessage(response.responseJSON.message, true);
+      },
+      complete: function() {
+        $btn.button('reset');
+      }
+   });
+    return false;
+  });
+  $('#feedbackForm input').change(function () {
+    var asteriskSpan = $(this).siblings('.glyphicon-asterisk');
+    if ($(this).val()) {
+      asteriskSpan.css('color', '#00FF00');
+    } else {
+      asteriskSpan.css('color', 'black');
+    }
+  });
+});
+
+//namespace as not to pollute global namespace
+var contactForm = {
+  isValidEmail: function (email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  },
+  /**
+   * Validates that phone number has 10 digits.
+   *
+   * @param  {String}  phone phone number to validate
+   * @return {Boolean} if phone number is valid
+   */
+  isValidPhone: function (phone) {
+    phone = phone.replace(/[^0-9]/g, '');
+    return (phone.length === 10);
+  },
+  clearErrors: function () {
+    $('#emailAlert').remove();
+    $('#feedbackForm .help-block').hide();
+    $('#feedbackForm .form-group').removeClass('has-error');
+  },
+  clearForm: function () {
+    $('.glyphicon-asterisk').css('color', 'black');
+    $('#feedbackForm input,textarea').val("");
+  },
+  addError: function ($input) {
+    $input.siblings('.help-block').show();
+    $input.parent('.form-group').addClass('has-error');
+  },
+  addAjaxMessage: function(msg, isError) {
+    $("#feedbackSubmit").after('<div id="emailAlert" class="alert alert-' + (isError ? 'danger' : 'success') + '" style="margin-top: 5px;">' + $('<div/>').text(msg).html() + '</div>');
+  }
+};
