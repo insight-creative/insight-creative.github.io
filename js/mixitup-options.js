581bd170edb34312c5b691ff2c5bf0096620bcf1
Kevin McGillivray <kmcgillivray@insightcreative.com>
2014-07-01 15:00:52 -0500

    Build mobile filter for work page, adjust filter details.

--- js/mixitup-options.js
+++ js/mixitup-options.js
@@ -4,6 +4,18 @@ jQuery(function(){
 
 	// Instantiate MixItUp:
 
-	$('#Container').mixItUp();
+	$('#Container').mixItUp({
+  	animation: {
+  		duration: 300,
+  		effects: 'fade',
+  		easing: 'ease'
+  	}
+	});
+  
+  // Make mobile filter work
+
+  $('#small-filter').on('change', function() { 
+     $('#Container').mixItUp('filter',this.value);
+  });
 
 });
